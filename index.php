821a5baf85ea433fb9c091c5770f3f56f1bd5d62
Cezar Leigos <cezarleigos28@outlook.com>
2021-03-19 12:40:02 -0300

    v0.01 - Criando estruturas do projeto
    Dessa vez nós começamos a criar as estruturas do projeto para que possamos começar dar forma ao projeto.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edite Codex</title>
+	<link rel="stylesheet" type="text/css" href="app/css/style.css">
+	<link rel="stylesheet" type="text/css" href="app/css/mobile.css">
+	<link rel="stylesheet" type="text/css" href="app/css/desktop.css">
+</head>
+<body>
+
+	<script type="text/javascript" src="app/js/script.js"></script>
+</body>
+</html>
