1483a4ec8f8e94b223c4c74317c4d67c27cb3813
Cezar Leigos <cezarleigos28@outlook.com>
2021-03-20 15:17:27 -0300

    v0.02 - Melhorando estruturas do projeto
    Dessa vez nós começamos a dar um pouco mais de forma em nosso projeto, colocamos como os objetos ficarão no painel, o lado esquerdo do painel vai conter os arquivos do projeto e os arquivos abertos no editor e do outro lado teremos o código do editor.

--- index.php
+++ index.php
@@ -8,6 +8,51 @@
 </head>
 <body>
 
+	<div class="painel">
+		<div class="painel-menu">
+			<div class="painel-item"></div>
+			<div class="painel-item"></div>
+			<div class="painel-item"></div>
+			<div class="painel-item"></div>
+			<div class="painel-item"></div>
+			<div class="painel-item"></div>
+		</div>
+		<div class="painel-group">
+			<div class="painel-left theme-background">
+
+			   <div class="painel-left-title theme-color-c">OPEN FILES</div>
+
+               <div class="painel-left-group">
+                  <div class="painel-left-item theme-color-d">Elemento</div>
+                  <div class="painel-left-close">&#10006;</div>
+               </div>
+               <div class="painel-left-group">
+                  <div class="painel-left-item theme-color-d">Elemento</div>
+                  <div class="painel-left-close">&#10006;</div>
+               </div>
+               <div class="painel-left-group">
+                  <div class="painel-left-item theme-color-d">Elemento</div>
+                  <div class="painel-left-close">&#10006;</div>
+               </div>
+               <div class="painel-left-group">
+                  <div class="painel-left-item theme-color-d">Elemento</div>
+                  <div class="painel-left-close">&#10006;</div>
+               </div>
+               <div class="painel-left-group">
+                  <div class="painel-left-item theme-color-d">Elemento</div>
+                  <div class="painel-left-close">&#10006;</div>
+               </div>
+               
+               <div class="painel-left-title theme-color-c">FOLDERS</div>
+
+			</div>
+			<div class="painel-right theme-background-b theme-color-e" contentEditable="true" spellcheck="false">
+               bbbb
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="app/js/jquery-3.6.0.min.js"></script>
 	<script type="text/javascript" src="app/js/script.js"></script>
 </body>
 </html>
