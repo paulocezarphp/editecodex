e1e5fe43a96f7c4379f54b678baf6c133d5daf51
Cezar Leigos <cezarleigos28@outlook.com>
2021-03-21 15:41:06 -0300

    v0.03 - Criando listagem de arquivos
    Dessa vez nós criamos uma área reservada para os arquivos do projeto, ela ficará localizada no canto esquerdo da tela, ali ficará nossa listagem de arquivos, todos os arquivos criados no diretório do projeto serão exibidos nessa área.

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 	<title>Edite Codex</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="sortcut icon" href="app/images/icon.png" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="app/css/style.css">
 	<link rel="stylesheet" type="text/css" href="app/css/mobile.css">
 	<link rel="stylesheet" type="text/css" href="app/css/desktop.css">
@@ -44,9 +47,38 @@
                </div>
                
                <div class="painel-left-title theme-color-c">FOLDERS</div>
+               
+
+               <div class="folder">            	    
+                  	<div class="folder-item theme-color-d">
+                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
+                  	   arquive.php
+                  	</div>
+               </div>
+               <div class="folder">            	    
+                  	<div class="folder-item theme-color-d">
+                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
+                  	   arquive.php
+                  	</div>
+               </div>
+               <div class="folder">            	    
+                  	<div class="folder-item theme-color-d">
+                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
+                  	   arquive.php
+                  	</div>
+               </div>
+               <div class="folder">            	    
+                  	<div class="folder-item theme-color-d">
+                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
+                  	   arquive.php
+                  	</div>
+               </div>
+               
+
 
 			</div>
-			<div class="painel-right theme-background-b theme-color-e" contentEditable="true" spellcheck="false">
+			<div class="painel-right theme-background-b theme-color-e" 
+			contentEditable="true" spellcheck="false">
                bbbb
 			</div>
 		</div>
