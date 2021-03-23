cf885e946dff37c1f8864e27aaa8e0b7e130815d
Cezar Leigos <cezarleigos28@outlook.com>
2021-03-22 21:41:08 -0300

    v0.04 - Colocando menu de arquivos em abas
    Dessa vez nós colocamos um menu de abas para representar os arquivos que estão abertos para que o usuário possa manipular.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Edite Codex</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="sortcut icon" href="app/images/icon.png" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="app/css/style.css">
 	<link rel="stylesheet" type="text/css" href="app/css/mobile.css">
@@ -23,63 +23,89 @@
 		<div class="painel-group">
 			<div class="painel-left theme-background">
 
-			   <div class="painel-left-title theme-color-c">OPEN FILES</div>
+				<div class="painel-left-title theme-color-c">OPEN FILES</div>
 
-               <div class="painel-left-group">
-                  <div class="painel-left-item theme-color-d">Elemento</div>
-                  <div class="painel-left-close">&#10006;</div>
-               </div>
-               <div class="painel-left-group">
-                  <div class="painel-left-item theme-color-d">Elemento</div>
-                  <div class="painel-left-close">&#10006;</div>
-               </div>
-               <div class="painel-left-group">
-                  <div class="painel-left-item theme-color-d">Elemento</div>
-                  <div class="painel-left-close">&#10006;</div>
-               </div>
-               <div class="painel-left-group">
-                  <div class="painel-left-item theme-color-d">Elemento</div>
-                  <div class="painel-left-close">&#10006;</div>
-               </div>
-               <div class="painel-left-group">
-                  <div class="painel-left-item theme-color-d">Elemento</div>
-                  <div class="painel-left-close">&#10006;</div>
-               </div>
-               
-               <div class="painel-left-title theme-color-c">FOLDERS</div>
-               
+				<div class="painel-left-group">
+					<div class="painel-left-item theme-color-d">Elemento</div>
+					<div class="painel-left-close">&#10006;</div>
+				</div>
+				<div class="painel-left-group">
+					<div class="painel-left-item theme-color-d">Elemento</div>
+					<div class="painel-left-close">&#10006;</div>
+				</div>
+				<div class="painel-left-group">
+					<div class="painel-left-item theme-color-d">Elemento</div>
+					<div class="painel-left-close">&#10006;</div>
+				</div>
+				<div class="painel-left-group">
+					<div class="painel-left-item theme-color-d">Elemento</div>
+					<div class="painel-left-close">&#10006;</div>
+				</div>
+				<div class="painel-left-group">
+					<div class="painel-left-item theme-color-d">Elemento</div>
+					<div class="painel-left-close">&#10006;</div>
+				</div>
 
-               <div class="folder">            	    
-                  	<div class="folder-item theme-color-d">
-                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
-                  	   arquive.php
-                  	</div>
-               </div>
-               <div class="folder">            	    
-                  	<div class="folder-item theme-color-d">
-                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
-                  	   arquive.php
-                  	</div>
-               </div>
-               <div class="folder">            	    
-                  	<div class="folder-item theme-color-d">
-                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
-                  	   arquive.php
-                  	</div>
-               </div>
-               <div class="folder">            	    
-                  	<div class="folder-item theme-color-d">
-                  	   <div class="folder-icon-button theme-color-b">&#10148;</div>
-                  	   arquive.php
-                  	</div>
-               </div>
-               
+				<div class="painel-left-title theme-color-c">FOLDERS</div>
 
+
+				<div class="folder">            	    
+					<div class="folder-item theme-color-d">
+						<div class="folder-icon-button theme-color-b">&#10148;</div>
+						arquive.php
+					</div>
+				</div>
+				<div class="folder">            	    
+					<div class="folder-item theme-color-d">
+						<div class="folder-icon-button theme-color-b">&#10148;</div>
+						arquive.php
+					</div>
+				</div>
+				<div class="folder">            	    
+					<div class="folder-item theme-color-d">
+						<div class="folder-icon-button theme-color-b">&#10148;</div>
+						arquive.php
+					</div>
+				</div>
+				<div class="folder">            	    
+					<div class="folder-item theme-color-d">
+						<div class="folder-icon-button theme-color-b">&#10148;</div>
+						arquive.php
+					</div>
+				</div>
 
 			</div>
-			<div class="painel-right theme-background-b theme-color-e" 
-			contentEditable="true" spellcheck="false">
-               bbbb
+			<div class="theme-background-b theme-color-e">
+				<div class="tabmenu">
+					<div class="tabmenu-item">
+						<div class="tabmenu-group">
+							<div class="tabmenu-namefile">filebill.php</div>
+							<div class="tabmenu-close">&#10006;</div>
+						</div>
+					</div>
+					<div class="tabmenu-item">
+						<div class="tabmenu-group">
+							<div class="tabmenu-namefile">filebill.php</div>
+							<div class="tabmenu-close">&#10006;</div>
+						</div>
+					</div>
+					<div class="tabmenu-item">
+						<div class="tabmenu-group">
+							<div class="tabmenu-namefile">filebill.php</div>
+							<div class="tabmenu-close">&#10006;</div>
+						</div>
+					</div>
+					<div class="tabmenu-item">
+						<div class="tabmenu-group">
+							<div class="tabmenu-namefile">filebill.php</div>
+							<div class="tabmenu-close">&#10006;</div>
+						</div>
+					</div>
+
+				</div>
+				<div class="painel-right" contentEditable="true" spellcheck="false">
+					bbbb
+				</div>
 			</div>
 		</div>
 	</div>
