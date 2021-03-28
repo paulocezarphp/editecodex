d1b2e6a0d14dc5afe34f66f032fbfb275bee7307
Cezar Leigos <cezarleigos28@outlook.com>
2021-03-28 11:21:00 -0300

    v0.08 - Manipulação de arquivos
    Dessa vez nós criamos uma classe para que um pouco mais a frente possamos implementar o controle dos arquivos e pastas que o usuário estiver manipulando durante seus projetos.

---
+++ app/php/files.php
@@ -0,0 +1,44 @@
+<?php
+
+class Files{
+  
+   public function Create_File($name_file, $context_file, $path){
+
+   }
+
+   public function Save_File($context_file){
+
+   }
+   
+   public function Open_File($name_file, $path){
+
+   }
+
+   public function Rename_File($name_file, $path){
+
+   }
+
+   public function Delete_File($name_file, $path){
+
+   }
+
+   public function Create_Folder($name_folder, $path){
+
+   }
+
+   public function Delete_Folder($name_folder, $path){
+
+   }
+
+   public function Rename_Folder($name_folder, $path){
+
+   }
+
+   public function List_Directory($name_folder, $path){
+
+   }
+
+}
+
+
+?>
